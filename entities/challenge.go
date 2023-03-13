fd80a155b88c38a9c38ea29b13061aaae2138f0b
Fabucik <fanda.burkon@seznam.cz>
2023-03-13 12:55:07 +0100

    Added challenge description

--- entities/challenge.go
+++ entities/challenge.go
@@ -1,10 +1,11 @@
 package entities
 
 type Challenge struct {
-	Name   string          `json:"name"`
-	Files  []ChallengeFile `json:"files"`
-	Flag   string          `json:"flag"`
-	Points int             `json:"points"`
+	Name        string          `json:"name"`
+	Files       []ChallengeFile `json:"files"`
+	Flag        string          `json:"flag"`
+	Points      int             `json:"points"`
+	Description string          `json:"description"`
 }
 
 type ChallengeFile struct {
